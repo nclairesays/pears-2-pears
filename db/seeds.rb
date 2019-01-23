ee7e06a435f98939efa09bf56f11eb5ad2191659
claire <github email address>
2019-01-22 18:00:48 -0600

    added user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Quote.destroy_all
+Round.destroy_all
+
+mario = User.create(name: 'Mario')
+toad = User.create(name: 'Toad')
+luigi = User.create(name: 'Luigi')
+bowser = User.create(name: 'Bowser')
+captain_falcon = User.create(name: 'Captain Falcon')
+donkey_kong = User.create(name: 'Donkey Kong')
+fox = User.create(name: 'Fox')
+princess_peach = User.create(name: 'Princess Peach')
+ice_climbers = User.create(name: 'Ice Climbers')
+pikachu = User.create(name: 'Pikachu')
+kirby = User.create(name: 'Kirby')
+link = User.create(name: 'Link')
+yoshi = User.create(name: 'Yoshi')
+jiggly_puff = User.create(name: 'Jiggly Puff')
+ganondorf = User.create(name: 'Ganondorf')
+mewtwo = User.create(name: 'Mewtwo')
+ike = User.create(name: 'Ike')
+mr_game_watch = User.create(name: 'Mr. Game & Watch')
+
+
+
+
